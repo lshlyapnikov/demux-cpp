0f6d47b4b9c7cc014520498dad103ba4ba2b45c0
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2023-08-27 15:55:23 -0400

    WIP

---
+++ test/sequencer_test.cpp
@@ -0,0 +1,12 @@
+#define BOOST_TEST_MODULE sequencer_test
+#include <boost/test/unit_test.hpp>
+
+#include "../src/sequencer.h"
+
+using namespace boost::unit_test;
+
+BOOST_AUTO_TEST_CASE(free_test_function)
+/* Compare with void free_test_function() */
+{
+  BOOST_TEST(true /* test assertion */);
+}
