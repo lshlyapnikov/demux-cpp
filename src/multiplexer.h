2bc95e20ec2b5d6b680e70dd76dbf3b660631b60
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2023-10-11 20:17:13 -0400

    wip

--- src/multiplexer.h
+++ src/multiplexer.h
@@ -6,8 +6,13 @@
 #include <cassert>
 #include <cstdint>
 #include <cstring>
+#include <optional>
 
 namespace ShmSequencer {
+
+using std::uint64_t;
+
+// enum MultiplexerResult { Ok, FullBuffer, MemoryOverlap };
 
 /// @brief Packet. Access has to be synchronized with an external atomic guard.
 /// @tparam M max packet size.
@@ -16,14 +21,14 @@ class Packet {
  public:
   auto copy_from(const std::array<uint8_t, M>& src, const uint16_t src_size) noexcept -> void {
     assert(src_size <= M);
-    // std::memcpy(this->buffer_.data(), src.data(), src_size);
-    std::copy_n(std::begin(src), src_size, std::begin(this->buffer_));
+    std::memcpy(this->buffer_.data(), src.data(), src_size);
+    // std::copy_n(std::begin(src), src_size, std::begin(this->buffer_));
     this->size_ = src_size;
   }
 
   [[nodiscard]] auto copy_into(std::array<uint8_t, M>& dst) const noexcept -> uint16_t {
-    // std::memcpy(dst.data(), this->buffer_.data(), this->size_);
-    std::copy_n(std::begin(this->buffer_), this->size_, std::begin(dst));
+    std::memcpy(dst.data(), this->buffer_.data(), this->size_);
+    // std::copy_n(std::begin(this->buffer_), this->size_, std::begin(dst));
     return this->size_;
   }
 
@@ -39,13 +44,20 @@ class Packet {
 /// @tparam M max packet size.
 template <const size_t N, const uint16_t M>
 struct MultiplexerPublisher {
+  /// @brief Copies the packet data into the shared buffer.
+  /// @param packet that will be copied.
+  /// @return true if the packet was copied and there was enough space in the buffer.
   auto send(const Packet<M>& packet) noexcept -> bool {
+    if (packet.size == 0) {
+      return true;
+    }
     const size_t required_space = sizeof(packet.size) + packet.size;
     const size_t new_offset = this->offset + required_space;
     if (new_offset <= BUFFER_SIZE) {
-      void* dst_address = this->buffer.data() + this->offset;
+      const uint8_t* dst_address = this->buffer.data() + this->offset;
       std::memcpy(dst_address, &packet, required_space);
       this->offset = new_offset;
+      return true;
     } else {
       return false;
     }
@@ -64,16 +76,21 @@ struct MultiplexerPublisher {
 /// @tparam M max packet size.
 template <const size_t N, const uint16_t M>
 struct MultiplexerSubscriber {
-  auto receive(Packet<M>* packet) noexcept -> bool {
-    const size_t required_space = sizeof(packet->size) + packet->size;
-    const size_t new_offset = this->offset + required_space;
-    if (new_offset <= BUFFER_SIZE) {
-      void* dst_address = this->buffer.data + this->offset;
-      std::memcpy(dst_address, &packet, required_space);
-      this->offset = new_offset;
-    } else {
-      return false;
-    }
+  // auto receive(Packet<M>* packet) noexcept -> bool {
+  //   const size_t required_space = sizeof(packet->size) + packet->size;
+  //   const size_t new_offset = this->offset + required_space;
+  //   if (new_offset <= BUFFER_SIZE) {
+  //     void* dst_address = this->buffer.data + this->offset;
+  //     std::memcpy(dst_address, &packet, required_space);
+  //     this->offset = new_offset;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  auto receive() noexcept -> std::optional<const Packet<M>*> {
+    // test
+    return std::nullopt;
   }
 
  private:
