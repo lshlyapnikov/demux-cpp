fa76d1308b00fd5030a34a32a488497829b5be1d
Leonid Shlyapnikov <lshlyapnikov@gmail.com>
2023-10-16 21:28:50 -0400

    wip

--- test/multiplexer_test.cpp
+++ test/multiplexer_test.cpp
@@ -18,11 +18,11 @@ TEST(MultiplexerTest, Packet) {
   Packet<M> packet;
 
   const std::array<uint8_t, M> src = {1, 2, 3, 4, 5, 6, 7, 8};
-  packet.copy_from(src, 3);
+  packet.read_from(src, 3);
   ASSERT_EQ(3, packet.size());
 
   std::array<uint8_t, M> dst{0, 0, 0, 0, 0, 0, 0, 0};
-  const uint16_t result = packet.copy_into(&dst);
+  const uint16_t result = packet.write_into(&dst);
   ASSERT_EQ(result, 3);
 
   const std::array<uint8_t, M> expected = {1, 2, 3, 0, 0, 0, 0, 0};
@@ -30,14 +30,14 @@ TEST(MultiplexerTest, Packet) {
 }
 
 // NOLINTBEGIN(misc-include-cleaner,cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers)
-TEST(MultiplexerTest, RoundtripCopy) {
-  rc::check("copy_from followed by copy_into does not not lose any data",
+TEST(MultiplexerTest, RoundtripReadWrite) {
+  rc::check("Packet::read_from followed by Packet::write_into does not not lose any data",
             [](const std::array<uint8_t, 256>& a0, const uint8_t size) {
-              Packet<256> p0;
-              p0.copy_from(a0, size);
+              Packet<256> packet;
+              packet.read_from(a0, size);
 
               std::array<uint8_t, 256> a1{};
-              const uint16_t written_bytes = p0.copy_into(&a1);
+              const uint16_t written_bytes = packet.write_into(&a1);
 
               const auto data0 = vector<uint8_t>(std::begin(a0), std::begin(a0) + size);
               const auto data1 = vector<uint8_t>(std::begin(a1), std::begin(a1) + size);
